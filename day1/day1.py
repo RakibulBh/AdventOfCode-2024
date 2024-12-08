2620797ffbeba357a269636048b41a1eebe6aea9
Rakibul Bhuiyan <80211974+RakibulBh@users.noreply.github.com>
2024-12-08 22:10:47 +0000

    fix: performance fix

--- day1/day1.py
+++ day1/day1.py
@@ -9,17 +9,12 @@ heap_right = []
 heap_left = []
 
 for val in lines:
-    heapq.heappush(heap_left, int(val[0]))
-
-for val in lines:
     heapq.heappush(heap_right, int(val[1]))
+    heapq.heappush(heap_left, int(val[0]))
 
 res = 0
 
 while heap_left and heap_right:
-    ans = (int(heapq.heappop(heap_right)) - int(heapq.heappop(heap_left)))
-    if ans < 0:
-        ans = ans * -1
-    res += ans
+    res += abs(int(heapq.heappop(heap_right)) - int(heapq.heappop(heap_left)))
 
 print(res)    
